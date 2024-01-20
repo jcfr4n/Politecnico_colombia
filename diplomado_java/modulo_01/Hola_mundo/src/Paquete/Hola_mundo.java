f2b6e39c6ba0327c2503545fcd09bdd7f733167a
Juan Carlos Francisco M <jcfm73@gmail.com>
2024-01-20 21:47:00 +0000

    Guia didactica 1 y ejercicios varios

---
+++ diplomado_java/modulo_01/Hola_mundo/src/Paquete/Hola_mundo.java
@@ -0,0 +1,13 @@
+package Paquete;
+
+public class Hola_mundo {
+    public static void main (String[] args) {
+        /*
+        System.out.println("Hola Mundo");
+         */
+
+        String str = "Hola Mundo!!!";
+        System.out.println(str);
+
+    }
+}
